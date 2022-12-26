16b90484729f4af1d21c4d39483a22297c8b364f
Joe Pringle <joe-pringle@outlook.com>
2022-12-26 21:16:31 +0000

    Add Games partition to automount script

--- Manjaro_I3/system_prep/autoMount_Drives.sh
+++ Manjaro_I3/system_prep/autoMount_Drives.sh
@@ -9,6 +9,8 @@ sudo cp fstab fstab.backup
 
 # append new entry to fstab
 echo "UUID=264a4224-6078-4535-afd6-e56e7d8a00e8 /media/ExtraSSD   ext4   defaults 0 0" | sudo tee -a /etc/fstab
+echo "UUID=e955d6bb-556d-406b-8d40-f3f0c3e5dc3d /media/Games   ext4   defaults 0 0" | sudo tee -a /etc/fstab
+
 
 
 # reload systemctl
@@ -16,3 +18,6 @@ systemctl daemon-reload
 # mount drive NOW instead of waiting for reboot
 sudo mkdir /media/ExtraSSD
 sudo mount UUID=264a4224-6078-4535-afd6-e56e7d8a00e8 /media/ExtraSSD
+
+sudo mkdir /media/Games
+sudo mount UUID=e955d6bb-556d-406b-8d40-f3f0c3e5dc3d /media/Games
