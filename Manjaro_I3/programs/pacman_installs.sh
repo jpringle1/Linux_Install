a438a3f821d0f844d09a15e74d695a9b9efec8b2
joe pringle <joe-pringle@outlook.com>
2022-12-29 20:28:07 +0000

    Bugfix pacman install script - .local steam entry linked

--- Manjaro_I3/programs/pacman_installs.sh
+++ Manjaro_I3/programs/pacman_installs.sh
@@ -3,16 +3,19 @@
 # solaar
 # qemu
 
+echo "refreshing database"
+sudo pacman -Syu --noconfirm
+
 echo "configuring firefox"
-config_name=firefox
-link_dir_name=.mozilla
-full_link_dir=~/$link_dir_name/$config_name
-sudo rm -R $full_link_dir && sudo ln -s $dropbox_main_dir$config_source_dir/$config_name $full_link_dir
+mkdir ~/.mozilla
+sudo rm -R ~/.mozilla/firefox
+sudo ln -s /media/ExtraSSD/Dropbox/"PC files"/Projects/Tech/LiveConfigs/firefox ~/.mozilla/firefox
 
 echo "configuring steam"
-link_dir_name=.steam
-full_link_dir=~/$link_dir_name
-
-sudo rm -R $full_link_dir && sudo ln -s $dropbox_main_dir$config_source_dir/$config_name $full_link_dir
+sudo rm -R ~/.steam
+sudo ln -s /media/ExtraSSD/Dropbox/"PC files"/Projects/Tech/LiveConfigs/steam/.steam ~/.steam
+mkdir ~/.local
+mkdir ~/.local/share
+sudo ln -s /media/ExtraSSD/Dropbox/"PC files"/Projects/Tech/LiveConfigs/steam/.local/share/Steam ~/.local/share/Steam
 
 sudo pacman -S --noconfirm steam
