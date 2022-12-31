8c25ec6283dd43aa0d2de615f601742d927599cd
joe pringle <joe@git.com>
2022-12-31 11:16:17 +0000

    Steam config - save library

--- Manjaro_I3/programs/steam_configure.sh
+++ Manjaro_I3/programs/steam_configure.sh
@@ -4,10 +4,13 @@ echo "configuring steam"
 remote_link=~/.local/share/Steam/userdata/171680367/7/remote
 config_shared_link=~/.local/share/Steam/userdata/171680367/config
 config_link=~/.local/share/Steam/config
+compatd_link=~/.local/share/Steam/compatibilitytools.d
+
 
 mkdir -p $remote_link
 mkdir -p $config_shared_link
 mkdir -p $config_link
+
 
 config_dir=/media/ExtraSSD/Dropbox/PC_files/Projects/Tech/LiveConfigs/steam/config.vdf
 config_link=~/.local/share/Steam/config/config.vdf
@@ -21,6 +24,8 @@ localconfig_link=~/.local/share/Steam/userdata/171680367/config/localconfig.vdf
 sharedconfig_dir=/media/ExtraSSD/Dropbox/PC_files/Projects/Tech/LiveConfigs/steam/sharedconfig.vdf
 sharedconfig_link=~/.local/share/Steam/userdata/171680367/7/remote/sharedconfig.vdf
 
+compatd_source=/media/ExtraSSD/Dropbox/PC_files/Projects/Tech/LiveConfigs/steam/compatibilitytools.d
+
 sudo rm -R $config_link
 sudo ln -s $config_dir $config_link
 sudo rm -R $libraryfolders_link
@@ -29,6 +34,8 @@ sudo rm -R $localconfig_link
 sudo ln -s $localconfig_dir $localconfig_link
 sudo rm -R $sharedconfig_link
 sudo ln -s $sharedconfig_dir $sharedconfig_link
+sudo rm -R $compatd_link
+sudo ln -s $compatd_source $compatd_link
 
 sudo pacman -S --noconfirm steam
 
