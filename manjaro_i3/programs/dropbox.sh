f314b8690a8372fb2127bec7a10a17e8925a0657
Joe Pringle <joe-pringle@outlook.com>
2023-01-15 13:11:29 +0000

    move dropbox autostart instruction into main dropbox script

--- manjaro_i3/programs/dropbox.sh
+++ manjaro_i3/programs/dropbox.sh
@@ -3,6 +3,7 @@
 #install dropbox daemon
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
-
+#autostart dropbox daemon
+sudo cp resources/dropboxd_desktop_entry ~/.config/autostart/dropboxd.desktop
 
 #add dropbox cli script to path or something
