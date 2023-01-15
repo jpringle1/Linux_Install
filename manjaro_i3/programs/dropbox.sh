6d1672c778ce4c671bde561801f6beccb8a2edc3
Joe Pringle <joe-pringle@outlook.com>
2023-01-15 13:31:07 +0000

    integrate dropbox installation

--- manjaro_i3/programs/dropbox.sh
+++ manjaro_i3/programs/dropbox.sh
@@ -4,6 +4,10 @@
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 #autostart dropbox daemon
-sudo cp resources/dropboxd_desktop_entry ~/.config/autostart/dropboxd.desktop
+sudo cp resources/dropboxd ~/.config/autostart/dropboxd
+#run dropbox
+cd resources
+./dropboxd
+cd ..
 
 #add dropbox cli script to path or something
