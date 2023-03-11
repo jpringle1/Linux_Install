f0adca509d359728b8dfc9fbed9a7d1c97a24f3c
Joe Pringle <joe-pringle@outlook.com>
2023-03-11 18:50:55 +0000

    Disable auto screen locking

--- manjaro_i3/system_prep/system_ui.sh
+++ manjaro_i3/system_prep/system_ui.sh
@@ -9,3 +9,10 @@ insertText="offerShutdown=false"
 var=$(grep -n "\[General\]" ksmserverrc | cut -f1 -d:)
 var=$((var+1))
 sed -i "${var}i ${insertText}" ~/.config/ksmserverrc
+
+# Remove autologout
+
+insertText="Autolock=false"
+var=$(grep -n "\[Daemon\]" kscreenlockerrc | cut -f1 -d:)
+var=$((var+1))
+sed -i "${var}i ${insertText}" ~/.config/kscreenlockerrc
