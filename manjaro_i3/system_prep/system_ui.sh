47a3f3f2ec8236e7e2bf0b9a6e3a6610a1c6775f
Joe Pringle <joe-pringle@outlook.com>
2023-03-11 18:17:14 +0000

    Remove shutdown options

--- manjaro_i3/system_prep/system_ui.sh
+++ manjaro_i3/system_prep/system_ui.sh
@@ -3,3 +3,9 @@
 desktopTheme=org.manjaro.breath-dark.desktop
 lookandfeeltool -a org.manjaro.breath-dark.desktop
 echo "Set global theme to $desktopTheme"
+
+#remove shutdown options
+insertText="offerShutdown=false"
+var=$(grep -n "\[General\]" ksmserverrc | cut -f1 -d:)
+var=$((var+1))
+sed -i "${var}i ${insertText}" ~/.config/ksmserverrc
