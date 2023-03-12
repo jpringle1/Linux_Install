855730d87e1c24e66c46f09eda1f9f3ea2694d51
Joe Pringle <joe-pringle@outlook.com>
2023-03-12 10:23:56 +0000

    Fix pipewire install

--- manjaro_i3/system_post/install_pipewire.sh
+++ manjaro_i3/system_post/install_pipewire.sh
@@ -1,15 +1,14 @@
 echo "install pipewire"
-sudo pacman -Rdd --noconfirm \
-manjaro-pulse \
-pulseaudio \
-pulseaudio-alsa \
-pulseaudio-zeroconf \
-pulseaudio-bluetooth \
-pulseaudio-ctl \
-sof-firmware
+sudo pacman -Rdd --noconfirm manjaro-pulse
+sudo pacman -Rdd --noconfirm pulseaudio
+sudo pacman -Rdd --noconfirm pulseaudio-alsa
+sudo pacman -Rdd --noconfirm pulseaudio-zeroconf
+sudo pacman -Rdd --noconfirm pulseaudio-bluetooth
+sudo pacman -Rdd --noconfirm pulseaudio-ctl
+sudo pacman -Rdd --noconfirm sof-firmware
 
 # warning: The previous command will likely return some "cannot find package" results. Ignore and move on to next command.
-
+sudo pacman -S --noconfirm pipewire
 sudo pacman -S --noconfirm manjaro-pipewire
 systemctl --user enable pipewire.socket --now
 systemctl --user start pipewire.service
