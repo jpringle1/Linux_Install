3f19bcbd29afe07a544f9b91e0085398e9b647c6
Joe Pringle <joe.pringle@proton.me>
2024-07-06 17:51:32 +0100

    Rename github token file

--- manjaro_i3/programs/installPrograms.sh
+++ manjaro_i3/programs/installPrograms.sh
@@ -8,7 +8,7 @@ sudo apt -y install gh #install github cli
 sudo apt install -y python3 python3-git #install python
 git config --global user.email "joe.pringle@proton.me"
 git config --global user.name "Joe Pringle"
-gh auth login --with-token < mytoken.txt ##setup github cli
+gh auth login --with-token < myGithubToken.txt #setup github cli
 sudo apt -y install flatpak #install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo # add flathub repo to flatpak
 
