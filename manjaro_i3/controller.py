22fe3a6891c3bfe7147b1e30ca80a6e08cfa3736
Joe Pringle <joe.pringle@proton.me>
2024-07-06 17:59:13 +0100

    semi fix

--- manjaro_i3/controller.py
+++ manjaro_i3/controller.py
@@ -1,13 +1,20 @@
 import yaml, os
 from pathlib import Path
 
+cwd = os.getcwd()
+
 with open("scriptOrder.yaml", 'r') as stream:
     scriptOrder = yaml.safe_load(stream)
 
 for directory in scriptOrder:
-    for script in directory:
+    print("in directory:", directory["directory"])
+    for script in directory["scripts"]:
+        print("running script:", script )
         if script[-2:] == "sh":
-            os.popen(f'./{script}')
+            os.popen(f'.{cwd}/{directory}/{script}')
         elif script[-2:] == "py":
-            exec(open(script).read())
+            path = cwd + "/" + directory["directory"] + "/" + script
+            print(path)
+            exec(open(path).read())
 
+# Next to do: refactor all files into OOP, aka turn them into libraries and or classes that the main file can use as methods. Will need to think about how i'm going to handle the config files'
