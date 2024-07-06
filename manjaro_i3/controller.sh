f1b4b343d8bc381b8466e1e7c7c9452fb1544275
Joe Pringle <joe.pringle@proton.me>
2024-07-06 17:55:34 +0100

    Add python to prep install steps

--- manjaro_i3/controller.sh
+++ manjaro_i3/controller.sh
@@ -6,4 +6,7 @@ su - #TODO: make so the next lines run automatically after entering root user
 sudo usermod -aG sudo joep
 su joep
 
+sudo apt install pip
+sudo apt install python3-yaml
+
 sudo python3 controller.py
