574ba83eb19f92f1904e9820789f2199da3cbf71
Joe Pringle <joe.pringle@proton.me>
2024-07-07 14:35:47 +0100

    importYaml

--- manjaro_i3/controller.py
+++ manjaro_i3/controller.py
@@ -4,22 +4,13 @@ from shutil import copyfile
 
 cwd = os.getcwd()
 
-with open("scriptOrder.yaml", 'r') as stream:
-    scriptOrder = yaml.safe_load(stream)
-
-for directory in scriptOrder:
-    print("in directory:", directory["directory"])
-    for script in directory["scripts"]:
-        print("running script:", script )
-        if script[-2:] == "sh":
-            os.popen(f'.{cwd}/{directory}/{script}')
-        elif script[-2:] == "py":
-            path = cwd + "/" + directory["directory"] + "/" + script
-            print(path)
-            exec(open(path).read())
-
 # Next to do: refactor all files into OOP, aka turn them into libraries and or classes that the main file can use as methods. Will need to think about how i'm going to handle the config files'
 
+class FileManaging:
+    @staticmethod
+    def importYaml(yamlFile, directory):
+        with open(cwd + directory + yamlFile, 'r') as stream:
+            return yaml.safe_load(stream)
 
 class DriveMounting:
 
