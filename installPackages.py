f1eaa52f51f27c97af801afd94f934fdd309a9f4
Joe Pringle <joe.pringle@proton.me>
2024-07-07 15:18:36 +0100

    Refactor packages

--- installPackages.py
+++
@@ -1,8 +0,0 @@
-for manager in packages:
-    for package in manager["apt"]:
-        os.popen(f"sudo apt -y install {package}")
-    for package in manager["deb"]:
-        os.popen(f'gh release download --pattern "*.deb" --repo https://github.com/{package}')
-        os.popen("mv *.deb temp.deb && sudo dpkg -i temp.deb && rm temp.deb")
-    for package in manager["flatpak"]:
-        os.popen(f"flatpak -y install {package}")
--- manjaro_i3/controller.py
+++ manjaro_i3/controller.py
@@ -6,14 +6,14 @@ cwd = os.getcwd()
 
 class FileManaging:
     @staticmethod
-    def importYaml(fileName, directory):
+    def importYaml(directory, fileName):
         with open(cwd + directory + fileName + ".yaml", 'r') as stream:
             return yaml.safe_load(stream)
 
 class Preqrequisites:
     @staticmethod
     def installAndConfigureGit(configDirectory, gitConfigYaml):
-        gitConfig = FileManaging.importYaml(configDirectory + "/" + gitConfigYaml + ".yaml")
+        gitConfig = FileManaging.importYaml(configDirectory, gitConfigYaml)
         Packages.installZypperPackage("git-core, gh")
         os.popen(f'git config --global user.email {gitConfig["email"]}')
         os.popen(f'git config --global user.name {gitConfig["name"]}')
@@ -41,8 +41,8 @@ class DriveMounting:
         return f'/dev/{drive} /mnt/{mountPoint} ext4 _netdev,rw 0 0'
     
     @classmethod
-    def mountDrives(cls, drivesYaml):
-        drives = FileManaging.importYaml(drivesYaml)
+    def mountDrives(cls, drivesDirectory, drivesYaml):
+        drives = FileManaging.importYaml(drivesDirectory, drivesYaml)
         
         copyfile(cwd + "/driveMounting/.smbcredentials", "/home/joep/.smbcredentials")
 
@@ -61,9 +61,31 @@ class DriveMounting:
         os.popen("systemctl daemon-reload && sudo mount -a")
 
 class Packages:
+    @staticmethod
+    def addRepository():
+        print("")
 
-sudo zypper -y ref # update and upgrade repos and packages
+    @staticmethod
+    def refreshRepositories():
+        os.popen("sudo zypper -y ref")
 
+    @staticmethod
+    def installZypperPackage(package):
+        os.popen(f"sudo zypper -y in {package}")
+
+    @staticmethod
+    def installFlatpak(package):
+        os.popen(f"flatpak -y install {package}")
+
+    @classmethod
+    def installPackages(cls, packagesDirectory, packagesYamlFile):
+        packages = FileManaging.importYaml(packagesDirectory, packagesYamlFile)
+        cls.addRepository()
+        cls.refreshRepositories()
+        for package in packages["zypper"]:
+            cls.installZypperPackage(package)
+        for package in packages["flatpak"]:
+            cls.installFlatpakPackage(package)
 
 class SystemLinks:
 
