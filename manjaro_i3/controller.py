605e5a7c669efa918a935ca6bc1f08fd0d260d81
Joe Pringle <joe.pringle@proton.me>
2024-07-07 15:48:38 +0100

    Refactor file structure

--- manjaro_i3/controller.py
+++ manjaro_i3/controller.py
@@ -6,19 +6,19 @@ cwd = os.getcwd()
 
 class FileManaging:
     @staticmethod
-    def importYaml(directory, fileName):
-        with open(cwd + directory + fileName + ".yaml", 'r') as stream:
+    def importYaml(fileName):
+        with open(cwd + "/resources/" + fileName + ".yaml", 'r') as stream:
             return yaml.safe_load(stream)
 
 
 class Preqrequisites:
     @staticmethod
-    def installAndConfigureGit(configDirectory, gitConfigYaml):
-        gitConfig = FileManaging.importYaml(configDirectory, gitConfigYaml)
+    def installAndConfigureGit(gitConfigDirectory, gitConfigYaml, githubTokenFile):
+        gitConfig = FileManaging.importYaml(gitConfigDirectory + "/" + gitConfigYaml)
         Packages.installZypperPackage("git-core, gh")
         os.popen(f'git config --global user.email {gitConfig["email"]}')
         os.popen(f'git config --global user.name {gitConfig["name"]}')
-        os.popen(f'gh auth login --with-token < {configDirectory}/myGithubToken.txt')
+        os.popen(f'gh auth login --with-token < {gitConfigDirectory}/{githubTokenFile}')
 
 class DriveMounting:
     @staticmethod
@@ -42,8 +42,8 @@ class DriveMounting:
         return f'/dev/{drive} /mnt/{mountPoint} ext4 _netdev,rw 0 0'
     
     @classmethod
-    def mountDrives(cls, drivesDirectory, drivesYaml):
-        drives = FileManaging.importYaml(drivesDirectory, drivesYaml)
+    def mountDrives(cls, drivesYaml):
+        drives = FileManaging.importYaml(drivesYaml)
         
         copyfile(cwd + "/driveMounting/.smbcredentials", "/home/joep/.smbcredentials")
 
@@ -79,8 +79,8 @@ class Packages:
         os.popen(f"flatpak -y install {package}")
 
     @classmethod
-    def installPackages(cls, packagesDirectory, packagesYamlFile):
-        packages = FileManaging.importYaml(packagesDirectory, packagesYamlFile)
+    def installPackages(cls, packagesYamlFile):
+        packages = FileManaging.importYaml(packagesYamlFile)
         cls.addRepository()
         cls.refreshRepositories()
         for package in packages["zypper"]:
