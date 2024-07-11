379ed8a7839fd708fbf4ec284869a0b96e4e2995
Joe <joe.pringle@proton.me>
2024-07-11 22:16:19 +0100

    Refactor FolderSyncing

--- manjaro_i3/FolderSyncing.py
+++ manjaro_i3/FolderSyncing.py
@@ -1,2 +1,6 @@
-class FolderSyncing:
-    print("not implemented")
+def syncKeyboardShortcuts():
+    link_target = "/home/joep/.config/kglobalshortcutsrc"
+    # ctrl+alt+arrow = move to beginning/end of line (Home/End)
+    # ctrl+shift+alt+arrow =select from cursor to beginning/end of line (Shift+Home/End)
+    # meta+space = krunner
+    # klipper - bind to mod+v
--- manjaro_i3/main.py
+++ manjaro_i3/main.py
@@ -5,9 +5,9 @@ import Packages
 import SystemLinks
 import RemoveShutdownOptions
 import Themes
+import FolderSyncing
 
 resourcesDir = os.getcwd() + "/resources/"
-
 
 Prerequisites.installAndConfigureGit(resourcesDir + "git")
 DriveMounting.mountDrives(resourcesDir + "drives", resourcesDir + "serverConfig")
@@ -15,11 +15,11 @@ Packages.installPackages(resourcesDir + "packages")
 SystemLinks.addAllSymlinks(resourcesDir + "symlinks")
 RemoveShutdownOptions.removeShutdownOptions()
 Themes.applyGrubTheme()
+FolderSyncing.syncKeyboardShortcuts()
 
 # TODO: 
 # - install iscsitools before mountDrives
 # - setup iscsi drive on boot (currently fstab entry bricks system)
 # - setup symlinks
 # - setup folder syncs
-# - refactor bash scripts to python (kde, key_bindings, removeShutdownOptions)
 # - use models
