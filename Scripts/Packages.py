1c492d8f82aa7945a08d498268f04007f6153529
Joe <joe.pringle@proton.me>
2024-07-18 20:57:00 +0100

    Refactor packages tests

--- Scripts/Packages.py
+++ Scripts/Packages.py
@@ -21,21 +21,12 @@ class Package:
     def installPackage(self):
         match self.repository:
             case Repository.Zypper:
-                self.zypperInstallPackage(self.packageName)
+                Subprocesses.zypperInstallPackage(self.packageName)
             case Repository.Flatpak:
-                self.flatpakInstallPackage(self.packageName)
-
-    def zypperInstallPackage(self, packageName):
-        subprocess.run(["sudo", "zypper", "in", "-y", packageName], check=True)
-
-    def flatpakInstallPackage(self, packageName):
-        subprocess.run(["flatpak", "-y", "install", packageName], check=True)
+                Subprocesses.flatpakInstallPackage(self.packageName)
                 
 class Packages:
-    def __init__(
-            self, 
-            json_data: List[dict]) -> None:
-        
+    def __init__(self, json_data: List[dict]) -> None:
         self.packages: List[Package] = []
 
         for entry in json_data:
@@ -49,4 +40,23 @@ class Packages:
             package.installPackage()
 
     def refreshRepositories(self):
-        subprocess.run(["sudo", "zypper", "--gpg-auto-import-keys" ,"ref"], check=True)
+        Subprocesses.refreshRepositories()
+
+    def __getitem__(self, index: int) -> Package:
+        return self.packages[index]
+
+    def __len__(self) -> int:
+        return len(self.packages)
+
+    def __str__(self):
+        return f"Packages({self.packages})"
+
+class Subprocesses:
+    def zypperInstallPackage(self, packageName):
+        subprocess.run(["sudo", "zypper", "in", "-y", packageName], check=True)
+
+    def flatpakInstallPackage(self, packageName):
+        subprocess.run(["flatpak", "-y", "install", packageName], check=True)
+
+    def refreshRepositories(self):
+        subprocess.run(["sudo", "zypper", "--gpg-auto-import-keys", "ref"], check=True)
--- Tests/Packages_test.py
+++ Tests/Packages_test.py
@@ -1,77 +1,81 @@
 import unittest
 from unittest.mock import Mock, mock_open
-from Scripts.Packages import Package, Repository, Packages
+from Scripts.Packages import Package, Repository, Packages, Subprocesses
 
 import unittest
-from unittest.mock import patch, call
+from unittest.mock import patch
 
 class TestPackage(unittest.TestCase):
-    @patch('subprocess.run')
-    def test_zypper_install_package(self, mock_run):
-        # Arrange
-        packageName = "test-package"
-        package = Package(packageName, "Zypper")
-        
-        # Act
+     
+    @patch.object(Subprocesses, 'zypperInstallPackage')
+    def test_installPackage_whenGivenZypperPackage_callsZypperInstallPackage(self, mock_zypperInstallPackage):
+        packageName = "package-name"
+        repository = "Zypper"
+        package = Package(packageName, repository)
+
         package.installPackage()
-        
-        # Assert
-        mock_run.assert_called_once_with(["sudo", "zypper", "in", "-y", packageName], check=True)
-    
-    @patch('subprocess.run')
-    def test_flatpak_install_package(self, mock_run):
-        # Arrange
-        packageName = "test-package"
-        package = Package(packageName, "Flatpak")
-        
-        # Act
+
+        mock_zypperInstallPackage.assert_called_once_with(packageName)
+
+    @patch.object(Subprocesses, 'flatpakInstallPackage')
+    def test_installPackage_whenGivenFlatpakPackage_callsFlatpakInstallPackage(self, mock_flatpakInstallPackage):
+        packageName = "package-name"
+        repository = "Flatpak"
+        package = Package(packageName, repository)
+
         package.installPackage()
-        
-        # Assert
-        mock_run.assert_called_once_with(["flatpak", "-y", "install", packageName], check=True)
+
+        mock_flatpakInstallPackage.assert_called_once_with(packageName)
 
 class TestPackages(unittest.TestCase):
-    def test_init(self):
-        # Arrange
-        package_data = [
+    def create_package_json(self):
+        return [
             {"repository": "Zypper", "packages": ["pkg1", "pkg2"]},
             {"repository": "Flatpak", "packages": ["pkg3"]}
         ]
 
-        # Act
+    def test_init_importsJson(self):
+        package_data = self.create_package_json()
+
         packages = Packages(package_data)
-        
-        # Assert
-        self.assertEqual(len(packages.packages), 3)
-        self.assertEqual(packages.packages[0].packageName, "pkg1")
-        self.assertEqual(packages.packages[0].repository, Repository.Zypper)
-        self.assertEqual(packages.packages[2].packageName, "pkg3")
-        self.assertEqual(packages.packages[2].repository, Repository.Flatpak)
+
+        self.assertEqual(len(packages), 3)
+        self.assertEqual(packages[0].packageName, "pkg1")
+        self.assertEqual(packages[0].repository, Repository.Zypper)
+        self.assertEqual(packages[2].packageName, "pkg3")
+        self.assertEqual(packages[2].repository, Repository.Flatpak)
     
     @patch('Scripts.Packages.Package.installPackage')
-    def test_installPackages(self, mock_installPackage):
-        # Arrange
-        packages = Packages.__new__(Packages)
-        packages.packages = [
-            Package("pkg1", "Zypper"),
-            Package("pkg2", "Flatpak")
-        ]
-        
-        # Act
+    def test_installPackages_t(self, mock_installPackage):
+        package_data = self.create_package_json()
+        packages = Packages(package_data)
+
         packages.installPackages()
         
-        # Assert
-        self.assertEqual(mock_installPackage.call_count, 2)
-        calls = [unittest.mock.call(), unittest.mock.call()]
-        mock_installPackage.assert_has_calls(calls, any_order=True)
+        self.assertEqual(mock_installPackage.call_count, 3)
+
+class TestSubprocesses(unittest.TestCase):
+    @patch('subprocess.run')
+    def test_refreshRepositories_refreshesRepositories(self, mock_run):
+        Subprocesses.refreshRepositories(self)
+        
+        mock_run.assert_called_once_with(["sudo", "zypper", "--gpg-auto-import-keys", "ref"], check=True)
+
+    @patch('subprocess.run')
+    def test_zypperInstallPackage_installsPackage(self, mock_run):
+        packageName = "test-package"
+
+        Subprocesses.zypperInstallPackage(self, packageName)
+        
+        mock_run.assert_called_once_with(["sudo", "zypper", "in", "-y", packageName], check=True)
     
     @patch('subprocess.run')
-    def test_refreshRepositories(self, mock_run):
-        # Arrange
-        packages = Packages.__new__(Packages)
+    def test_flatpakInstallPackage_installsPackage(self, mock_run):
+        packageName = "test-package"
         
-        # Act
-        packages.refreshRepositories()
+        Subprocesses.flatpakInstallPackage(self, packageName)
         
-        # Assert
-        mock_run.assert_called_once_with(["sudo", "zypper", "--gpg-auto-import-keys", "ref"], check=True)
+        mock_run.assert_called_once_with(["flatpak", "-y", "install", packageName], check=True)
+
+if __name__ == '__main__':
+    unittest.main()
